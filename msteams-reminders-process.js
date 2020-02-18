e9fbb8a0d62ed3f024d1351293c35a167537f4b8
John Sibly <John.Sibly@dazn.com>
2020-02-18 13:08:02 +0000

    Fix bug around lastReminderTime, tidy up code

--- msteams-reminders-process.js
+++ msteams-reminders-process.js
@@ -1,17 +1,16 @@
 const axios = require('axios');
 const parser = require('cron-parser');
 const AWS = require('aws-sdk');
-const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-2'});
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-2'}); //TODO: Move to config
 
 exports.handler = function(event, context, callback){
     console.log('processing event: %j', event);
 
     let scanningParameters = {
         TableName: 'msteams-reminders',
-        Limit: 1000 //maximum result of 1000 items
+        Limit: 1000
     };
 
-    //In dynamoDB scan looks through your entire table and fetches all data
     docClient.scan(scanningParameters, function(err,data){
         if(err){
             callback(err, null);
@@ -24,7 +23,7 @@ exports.handler = function(event, context, callback){
 
 async function processReminder(reminder) {
     const now = new Date();
-    let endDate = now;
+    let endDate = new Date(now);
     endDate.setHours(now.getHours()+1);
     
     let options = {
@@ -37,13 +36,11 @@ async function processReminder(reminder) {
     try {
       let nextExecutionTime = interval.next();
       let nextDateTime = new Date(nextExecutionTime.value);
-
-      console.log(`datetime=${now} nextExecutionTime=${nextExecutionTime.value}`)
-
       if (nextDateTime <= now) {
-        // we need to post to teams
         console.log(reminder.reminderMessage);
         reminder.lastTimeReminderExecuted = nextDateTime;
+        
+        await postReminderToTeams(reminder);
         
         const payload = {body: reminder};
         let lambda = new AWS.Lambda();
@@ -56,9 +53,6 @@ async function processReminder(reminder) {
           if (err) console.log(err, err.stack); // an error occurred
           else     console.log(data);           // successful response
         });
-
-        const posted = await postReminderToTeams(reminder);
-
       }
 
     } catch (e) {
@@ -74,7 +68,7 @@ async function postReminderToTeams(reminder) {
     summary: 'Reminder',
     sections: [
       {
-        activityTitle: `${reminder.reminderMessage}`,
+        activityTitle: `⏰ Reminder: ${reminder.lastTimeReminderExecuted.toLocaleTimeString('en-GB')} ⏰`,
       },
     ],
   };
