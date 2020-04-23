02ea61e4d97956f7cf80cdc6b9d7690c01992bde
John Sibly <John.Sibly@dazn.com>
2020-04-23 15:37:29 +0100

    Experiment formatting as an html table

--- getData.js
+++ getData.js
@@ -3,36 +3,34 @@ const time = [];
 const lastTime = [];
 const text = document.getElementById("text");
 
-function printMessages() {
-  message.forEach(message => text.innerHTML += message);
-}
-
-function printTime() {
-  time.forEach(time => text.innerHTML += time);
-}
-
-function printLastTimeExecuted() {
-  lastTime.forEach(previous => text.innerHTML += previous);
-}
-
 async function getReminders() {
   const Reminders = axios.get('https://dlgoyn6ebc.execute-api.eu-west-2.amazonaws.com/prod/msteams-reminders')
-
   const response = await Reminders;
-
   const info = response.data.Items;
   
   console.log(info);
+  printTable(info);
+}
 
-  info.forEach(item => message.push(item.reminderMessage))
+function printTable(info) {
+  let table = '<table style="width:100%">';
+  table += '<tr>';
+  table += '<th>Reminder Message</th>';
+  table += '<th>Cron Interval</th>';
+  table += '<th>Last Time Reminder Executed</th>';
+  table += '<th>Timezone</th>';
+  table += '</tr>';
 
-  info.forEach(item => time.push(item.cronInterval))
-
-  info.forEach(item => lastTime.push(item.lastTimeReminderExecuted))
-
-  printMessages();
-  printTime();
-  printLastTimeExecuted()
+  info.forEach(function(item) {
+    table += `<tr>`;
+    table += `<td>${item.reminderMessage}</td>`;
+    table += `<td>${item.cronInterval}</td>`;
+    table += `<td>${item.lastTimeReminderExecuted}</td>`;
+    table += `<td>${item.timeZone}</td>`;
+    table += `</tr>`;
+  });
+  table += '</table>';
+  text.innerHTML += table;
 }
 
 getReminders();
