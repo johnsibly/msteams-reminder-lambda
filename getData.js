ff45c50939cfc2de3648048bfc429f7253ba5000
CarlyJ88 <carly.jenkinson65@googlemail.com>
2020-05-08 15:03:20 +0100

    Data formatted into a table.

--- getData.js
+++ getData.js
@@ -1,55 +1,22 @@
-const message = [];
-const time = [];
-const lastTime = [];
-const text = document.getElementById("text");
-
-function printMessages() {
-  message.forEach(message => text.innerHTML += message);
-}
-
-function printTime() {
-  time.forEach(time => text.innerHTML += time);
-}
-
-function printLastTimeExecuted() {
-  lastTime.forEach(previous => text.innerHTML += previous);
-}
-
 async function getReminders() {
-  const Reminders = axios.get('https://dlgoyn6ebc.execute-api.eu-west-2.amazonaws.com/prod/msteams-reminders')
-
+  const Reminders = axios.get(
+    'https://dlgoyn6ebc.execute-api.eu-west-2.amazonaws.com/prod/msteams-reminders'
+  );
   const response = await Reminders;
-
   const info = response.data.Items;
-  
-  console.log(info);
-
-  info.forEach(item => message.push(item.reminderMessage))
-
-  info.forEach(item => time.push(item.cronInterval))
-
-  info.forEach(item => lastTime.push(item.lastTimeReminderExecuted))
-
-  printMessages();
-  printTime();
-  printLastTimeExecuted()
+  const table = document.getElementById('reminders');
+  info.forEach(item => {
+    let newRow = document.createElement('tr');
+    let newCell1 = document.createElement('td');
+    let newCell2 = document.createElement('td');
+    let newCell3 = document.createElement('td');
+    newCell1.innerText = item.reminderMessage;
+    newCell2.innerText = item.cronInterval;
+    newCell3.innerText = item.lastTimeReminderExecuted;
+    newRow.append(newCell1);
+    newRow.append(newCell2);
+    newRow.append(newCell3);
+    table.append(newRow);
+  });
 }
-
 getReminders();
-
-
-// do i want to access every items key individually? i.e. all the reminderMessage then all the cronInterval? include lastTimeReminderExecuted?
-
-// text.innerHTML += finalResult;
-
-// Object.defineProperties(info).forEach
-
-// console.log(typeof info)
-
-// data.push(info)
-
-// , console.log(JSON.stringify(item.reminderMessage))
-
-// , console.log(JSON.stringify(item.cronInterval))
-
-// , console.log(JSON.stringify(item.lastTimeReminderExecuted))
