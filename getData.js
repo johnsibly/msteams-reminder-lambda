4ba8b6cc7a02263d793a5aa8056633ce601b81e7
CarlyJ88 <carly.jenkinson65@googlemail.com>
2020-05-08 15:26:28 +0100

    Refactored function and added css.

--- getData.js
+++ getData.js
@@ -1,25 +1,31 @@
-function createTable(info) {
-  const table = document.getElementById('reminders');
-  info.forEach(item => {
-    let newRow = document.createElement('tr');
-    let newCell1 = document.createElement('td');
-    let newCell2 = document.createElement('td');
-    let newCell3 = document.createElement('td');
+function createTable (info) {
+  const table = document.getElementById ('reminders');
+  info.forEach (item => {
+    let newRow = document.createElement ('tr');
+    let newCell1 = document.createElement ('td');
+    let newCell2 = document.createElement ('td');
+    let newCell3 = document.createElement ('td');
     newCell1.innerText = item.reminderMessage;
     newCell2.innerText = item.cronInterval;
     newCell3.innerText = item.lastTimeReminderExecuted;
-    newRow.append(newCell1);
-    newRow.append(newCell2);
-    newRow.append(newCell3);
-    table.append(newRow);
+    newRow.append (newCell1);
+    newRow.append (newCell2);
+    newRow.append (newCell3);
+    table.append (newRow);
   });
 }
-async function getReminders() {
-  const Reminders = axios.get(
+
+async function getInfo() {
+  const Reminders = axios.get (
     'https://dlgoyn6ebc.execute-api.eu-west-2.amazonaws.com/prod/msteams-reminders'
   );
   const response = await Reminders;
   const info = response.data.Items;
+  return info;
+}
+
+async function getReminders() {
+  const info = await getInfo()
   createTable(info);
 }
 getReminders();
