2cbdf6df9ac1a2ed4e6e061057f3c6e18a23666d
John Sibly <John.Sibly@dazn.com>
2020-08-12 11:13:45 +0100

    Added logic to add a new reminder

--- reminders.js
+++ reminders.js
@@ -17,6 +17,8 @@ function printTable(info) {
   table += '<th>Cron Interval</th>';
   table += '<th>Last Time Reminder Executed</th>';
   table += '<th>Timezone</th>';
+  // table += '<th>Id</th>';
+  table += '<th>Delete</th>';
   table += '</tr>';
 
   info.forEach(function(item) {
@@ -25,12 +27,14 @@ function printTable(info) {
     table += `<td>${item.cronInterval}</td>`;
     table += `<td>${item.lastTimeReminderExecuted}</td>`;
     table += `<td>${item.timeZone}</td>`;
+    // table += `<td>${item.id}</td>`;
+    table += `<td><button type="button">Delete</button></td>`;
     table += `</tr>`;
   });
   table += '</table>';
   text.innerHTML += table;
 
-  addHandlers();
+  // addRowClickHandlers();
 }
 
 document.getElementById('submit').onclick = async function () {
@@ -38,14 +42,15 @@ document.getElementById('submit').onclick = async function () {
   const cron = document.getElementById("cron").value;
   const timeZone = document.getElementById("timeZone").value;
   const webHookUrl = document.getElementById("webHookUrl").value;
-  
+  // const id = document.getElementById("id").value;
+
   if (message.length == 0 || cron.length == 0 || timeZone.length == 0 || webHookUrl.length == 0) {
     window.alert("All fields must contain a value");
     return;
   }
 
   try {
-    const reminder = {"body": {"cronInterval": cron, "reminderMessage": message, "timeZone": "Europe/London", "teamsChannelWebhook": "https://mywebhookurl"}};
+    const reminder = {"body": {"cronInterval": cron, "reminderMessage": message, "timeZone": timeZone, "teamsChannelWebhook": webHookUrl}};
     console.log(reminder);
     
     const response = await axios.put(remindersUrl, reminder, {
@@ -53,15 +58,13 @@ document.getElementById('submit').onclick = async function () {
         'content-type': 'application/json',
       },
     });
-
-    return `${response.status} - ${response.statusText}`;
-    
+    console.log(response);
   } catch (err) {
     throw new Error(err);
   }
 }
 
-function addHandlers() {
+function addRowClickHandlers() {
   const table = document.getElementById("reminders");
   const rows = table.getElementsByTagName("tr");
   for (i = 0; i < rows.length; i++) {
@@ -70,13 +73,16 @@ function addHandlers() {
       return function() {
         const message = row.getElementsByTagName("td")[0].innerHTML;
         const cron = row.getElementsByTagName("td")[1].innerHTML;
+        const lastTime = row.getElementsByTagName("td")[2].innerHTML;
         const timeZone = row.getElementsByTagName("td")[3].innerHTML;
+        const id = row.getElementsByTagName("td")[4].innerHTML;
 
-        console.log(`${message}, ${cron}, ${lastTime}, ${timeZone}`);
+        console.log(`${message}, ${cron}, ${lastTime}, ${timeZone}, ${id}`);
 
         document.getElementById("message").value = message;
         document.getElementById("cron").value = cron;
         document.getElementById("timeZone").value = timeZone;
+        document.getElementById("id").value = id;
       };
     };
     currentRow.onclick = createClickHandler(currentRow);
@@ -86,7 +92,6 @@ function addHandlers() {
 
 function initialise() {
   getReminders();
-  
 }
 
 initialise();
