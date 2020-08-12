9196bf5c965dd29671df3449946dbe083d24551d
John Sibly <John.Sibly@dazn.com>
2020-08-12 16:19:27 +0100

    Fixed logic for delete statement

--- msteams-reminders-delete.js
+++ msteams-reminders-delete.js
@@ -6,13 +6,19 @@ exports.handler = (event, context, callback) => {
     const TableName = "msteams-reminders";
 
     const reminder = event.body;
-    const Item = {"id": reminder.id};
-    dynamo.deleteItem(Item, function(err, data) {
+    const item = {
+        Key: {
+          id: reminder.id
+        },
+        TableName: TableName,
+    };
+
+    dynamo.delete(item, function(err, data) {
         if (err) {
-            callback(`event: ${event}, err: ${err}`, null);
+            callback(`event: ${event} ${reminder.id}, err: ${err}`, null);
         }
         else {
-            callback(null, Item);
+            callback(null, item);
         }
     });
 };
