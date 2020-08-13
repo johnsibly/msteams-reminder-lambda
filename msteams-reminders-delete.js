07ee399fdbbfbf5b2c954309653bdfb122ff81e5
John Sibly <John.Sibly@dazn.com>
2020-08-13 14:26:40 +0100

    Log id of item being deleted in cloudwatch

--- msteams-reminders-delete.js
+++ msteams-reminders-delete.js
@@ -3,6 +3,7 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 AWS.config.update({region: `eu-west-2`});
 
 exports.handler = (event, context, callback) => {
+    console.info(event);
     const TableName = "msteams-reminders";
 
     const reminder = event.body;
